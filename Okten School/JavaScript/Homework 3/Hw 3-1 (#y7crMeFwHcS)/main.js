8185b29847c7d69883e1518c895f25c3c7489783
maksl <maks.livs.1996@gmail.com>
2024-11-11 18:12:08 +0100

    Gotowo

---
+++ Okten School/JavaScript/Homework 3/Hw 3-1 (#y7crMeFwHcS)/main.js
@@ -0,0 +1,7 @@
+
+
+// #y7crMeFwHcS
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+
+for (let i = 1; i < 11; i++){
+    document.write("<div>Poland<div/>");}
---
+++ Okten School/JavaScript/Homework 3/Hw 3-2 (#TYj7ncx)/main.js
@@ -0,0 +1,12 @@
+
+// #TYj7ncx
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+
+for (let i = 1; i < 11; i++) {
+
+    // ------Вариант 1-------
+    // document.write('<div>Poland ' + i + '<div/>');
+
+    // ------Вариант 2-------
+    // document.write(`<div>Poland ${i}</div>`);
+}
